fd450a09a6c6ea49b6e70293bb174c9840b43e4a
Omar Farooq <omarfarooqanis@outlook.com>
2026-08-02 18:45:51 +0530

    implementing leet code problem Find All Duplicates in an Array
    Added time and space complexity comments for clarity.

---
+++ Find All_Duplicates_in_an_Array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        duplicates = []
+
+        for num in nums:
+            index = abs(num) - 1
+
+            if nums[index] < 0:
+                duplicates.append(abs(num))
+
+            else :
+                nums[index] = -nums[index]
+
+        return duplicates
+
+# Time complexity : O(n)
+# Space Complexity: O(1)
